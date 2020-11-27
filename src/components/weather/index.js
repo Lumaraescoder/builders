b1fa418b363b20336bb19682b7566324433f9275
hash <hash@mefs.br>
2020-11-27 18:30:21 -0500

    weathekey and imports

--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import * as s from './style.js'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Weather = (props) => {
+const Weather = ({weather, temperature}) => {
   return (
     <s.view>
       <s.ViewContainer>
         <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
-        <s.Texts tempText>Temperatura˚</s.Texts>
+        <s.Texts tempText>{Temperatura}*</s.Texts>
       </s.ViewContainer>
       <s.ViewBodyContainer>
         <s.Texts> Muito cedo manhã</s.Texts>
