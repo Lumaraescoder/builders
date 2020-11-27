58a6931566953bcc65430d686dd459dd405dc560
hash <hash@mefs.br>
2020-11-27 17:26:32 -0500

    weater component working

--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -1,52 +1,22 @@
 import React from 'react';
-import { View, Text, Stylesheet } from 'react-native';
 import * as s from './style.js'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Weather = () => {
+const Weather = (props) => {
   return (
-    <s.View>
-      <View style={styles.headerContainer}>
+    <s.view>
+      <s.ViewContainer>
         <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
-        <Text style={styles.tempText}>Temperature˚</Text>
-      </View>
-      <View style={styles.bodyContainer}>
-        <Text style={styles.title}>So Sunny</Text>
-        <Text style={styles.subtitle}>It hurts my eyes!</Text>
-      </View>
-    </s.View>
+        <s.Texts tempText>Temperatura˚</s.Texts>
+      </s.ViewContainer>
+      <s.ViewBodyContainer>
+        <s.Texts> Muito cedo manhã</s.Texts>
+        <s.Texts subtitle>Volte a codar</s.Texts>
+      </s.ViewBodyContainer>
+    </s.view>
   );
 };
 
-const styles = StyleSheet.create({
-  weatherContainer: {
-    flex: 1,
-    backgroundColor: '#f7b733'
-  },
-  headerContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  tempText: {
-    fontSize: 48,
-    color: '#fff'
-  },
-  bodyContainer: {
-    flex: 2,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-end',
-    paddingLeft: 25,
-    marginBottom: 40
-  },
-  title: {
-    // fontSize: 48,
-    color: '#fff'
-  },
-  subtitle: {
-    fontSize: 24,
-    color: '#fff'
-  }
-});
 
 
 export default Weather;
--- src/components/weather/style.js
+++ src/components/weather/style.js
@@ -3,9 +3,33 @@ import { View, Text, Stylesheet } from 'react-native';
 
 
 export const view = styled.View`
+
     flex: 1;
     background-color: #f7b733;
-    
 
 `
+
+export const ViewContainer = styled.View`
+
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    
+`
+export const ViewBodyContainer = styled.View`
+    flex: 2;
+    align-items: flex-start;
+    justify-content: flex-end;
+    padding-left: 25px;
+    margin-bottom: 40px; 
+`
+export const Texts = styled.Text`
+    font-size: 48px;
+    color: #fff;
+    font-size: ${props => props.subtitle ? 48 : 24};
+    font-size: ${props => props.tempText ? 48 : 24};
+
+
+`
+
 
